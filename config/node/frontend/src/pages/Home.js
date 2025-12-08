884062b48eb94a05b27a4cafe9bfbfe5a89ee4b4
kubaz <kuba.zaluga@gmail.com>
2025-12-08 20:04:44 +0100

    feat: title and button

--- config/node/frontend/src/pages/Home.js
+++ config/node/frontend/src/pages/Home.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Button, Typography } from "@mui/material";
 
 function Home(props)
 {
     return(
-        <div>
-            strona internetowa
-            </div>
+        <div className="home">
+            <h1 className="home__header">GEOPORTAL</h1>
+            <Typography className="home__subtitle">
+                Geoportal tematyczny poświęcony danym przestrzennym.
+            </Typography>
+            
+            <Button className='home__button' variant='contained' size='large'> zXZ </Button>
+        </div>
     );
 }
 
