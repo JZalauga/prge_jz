763961f75e75ec32af7adfaf975de71264320d35
kubaz <kuba.zaluga@gmail.com>
2026-01-09 02:07:08 +0100

    .

--- config/fastapi/app/routers/db_insert.py
+++ config/fastapi/app/routers/db_insert.py
@@ -1,42 +1,44 @@
 from fastapi import APIRouter
-from sqlalchemy  import create_engine, text
+from sqlalchemy import create_engine, text
 
 from app.settings import db_name, db_user, db_password
 
+router_insert = APIRouter()
 
-router = APIRouter()
 
 def connect_to_db(db_name: str, db_user: str, db_password: str):
-	return create_engine(
-		f"postgresql://{db_user}: {db_password}@postgis: 5432/{db_name}"
-	)
-	
+    return create_engine(
+        f"postgresql://{db_user}:{db_password}@postgis:5432/{db_name}"
+    )
 
-@router.get("/insert_user")
+
+@router_insert.get("/insert_user")
 async def insert_user():
-	try:
-		db_connection = connect_to_db(db_name=db_name, db_user=db_user, db_password=db_password)
-		#params = {
-		 	#"name": request.name,
-		 	#"posts": request.posts,
-		 	#"location": request.location
-		#}
-		params = {
-			"name": "ss",
-			"posts": 67,
-			"location": "Warszawa"
-		}
-		insert_query = text('''
-INSERT INTO users(name, posts, location) VALUES (:name, :posts, :location);''')
-		with db_connection.connect() as conn:
-			result = conn.execute(insert_query, params)
-			conn.commit()
-			print(result)
+    try:
+        db_connection = connect_to_db(db_name=db_name, db_user=db_user, db_password=db_password)
 
-	except Exception as e:
-		print(e)
-		raise e
-	return
+        params = {
+            "name": "Asia",
+            "posts": 4,
+            "location": "Warszawa"
+        }
+
+        sql_query = text("""
+                         insert into users (name, posts, location)
+                         values (:name, :posts, :location); \
+                         """)
+
+        with db_connection.connect() as conn:
+            result = conn.execute(sql_query, params)
+            conn.commit()
+            print(result)
+
+
+    except Exception as e:
+        print(e)
+        raise e
+
+    return {"statu": 1}n
 
 
 """
