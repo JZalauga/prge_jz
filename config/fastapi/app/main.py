c603b3c96a46c94fa3e5a77b9b3cf49e687d4a58
kubaz <kuba.zaluga@gmail.com>
2026-02-08 22:05:41 +0100

    feat: cemetery delete backend

--- config/fastapi/app/main.py
+++ config/fastapi/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.routers.static_content import router
 from app.routers.db_insert import router_insert
 from app.routers.dynamic_content import router_get_cemeteries, router_get_workers, router_get_clients
+from app.routers.db_delete import router_delete
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="Mapbook API")
 
@@ -20,4 +21,5 @@ app.include_router(router_insert, prefix="/app")
 app.include_router(router_get_cemeteries, prefix="/app")
 app.include_router(router_get_workers, prefix="/app")
 app.include_router(router_get_clients, prefix="/app")
+app.include_router(router_delete, prefix="/app")
 
